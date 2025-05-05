897b1532af851e8e24066055add5efb23703d436
nalvac <vacna1997@gmail.com>
2025-05-05 10:36:39 +0200

    Initial commit

---
+++ ui-components/src/lib/button/button.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { ButtonComponent } from './button';
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const meta: Meta<ButtonComponent> = {
+  component: ButtonComponent,
+  title: 'ButtonComponent',
+};
+export default meta;
+type Story = StoryObj<ButtonComponent>;
+
+export const Primary: Story = {
+  args: {},
+};
+
+export const Heading: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await expect(canvas.getByText(/button works!/gi)).toBeTruthy();
+  },
+};
